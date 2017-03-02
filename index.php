a68505188c1bbd15047a4ccd78e53b493fad1222
philipakash <philipakash@yahoo.com>
2017-03-03 00:47:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
 
     <body>
 <div align="center">
-                <h1>Hello Cloud Guru's</h1>
-                <h2>Congratulations! You have successfully installed an application to OpsWorks!</h2>
+                <h1>Hello This is Akash here learning Ops Wor</h1>
+                <h2>Congratulations!</h2>
                 <div align="center"><img src="https://s3-eu-west-1.amazonaws.com/acloudguru/logo.png"></div>
                 <p>You ACloudGuru Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a new stack and layer in OpsWorks.</p>
                 <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
